0da3c21d7b412f09d29150436ffe88025fcaff72
AlcatrazALT <olegmazalevsky21@yandex.ru>
2020-12-06 09:33:18 +0700

    Move player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float horizontalInput;
+
+    [SerializeField]
+    private float speed = 10.0f;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+    }
+}
