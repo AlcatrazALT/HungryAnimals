f33e2e0865f4c757d6e2d672db158b0acfd1c353
AlcatrazALT <olegmazalevsky21@yandex.ru>
2020-12-06 10:51:01 +0700

    Spawn the animals at timed intervals

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,24 +5,32 @@ public class SpawnManager : MonoBehaviour
     [SerializeField]
     private GameObject[] animalPrefabs;
 
+    [SerializeField]
+    private float startDelay = 2;
+
+    [SerializeField]
+    private float spawnInterval = 1.5f;
+
     private float spawnRangeX = 10.0f;
     private float spawnPositionZ = 20.0f;
 
     // Start is called before the first frame update
     private void Start()
     {
+        InvokeRepeating(nameof(SpawnRandomAnimals), startDelay, spawnInterval);
     }
 
     // Update is called once per frame
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            var animalIndex = Random.Range(0, animalPrefabs.Length);
+    }
 
-            var spawnPosition = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPositionZ);
+    private void SpawnRandomAnimals()
+    {
+        var animalIndex = Random.Range(0, animalPrefabs.Length);
 
-            Instantiate(animalPrefabs[animalIndex], spawnPosition, animalPrefabs[animalIndex].transform.rotation);
-        }
+        var spawnPosition = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPositionZ);
+
+        Instantiate(animalPrefabs[animalIndex], spawnPosition, animalPrefabs[animalIndex].transform.rotation);
     }
 }
