923c632492f794483140fcc98ec0b2d848cd1fa6
AlcatrazALT <olegmazalevsky21@yandex.ru>
2020-12-06 11:05:58 +0700

    Destroy objects on collision

---
+++ Assets/Scripts/DetectCollisions.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class DetectCollisions : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+        Destroy(other.gameObject);
+    }
+}
