d895dae54ca4ae0fe7fda465a9a90ce3c124b558
AlcatrazALT <olegmazalevsky21@yandex.ru>
2020-12-06 10:08:52 +0700

    Destroy projectiles

---
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    private float topBound = 40.0f;
+    // Start is called before the first frame update
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (transform.position.z > topBound)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
