93647e609d2fdd52ae2837d3fb615d96b1e77fba
AlcatrazALT <olegmazalevsky21@yandex.ru>
2020-12-06 09:43:13 +0700

    Set the boundaries of the player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private float speed = 10.0f;
 
+    private float xRange = 10.0f;
+
     // Start is called before the first frame update
     private void Start()
     {
@@ -16,6 +18,16 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
     }
