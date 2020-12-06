a4c5d50d8dadb6b9b01192aa48b5a09fc9a37fb9
AlcatrazALT <olegmazalevsky21@yandex.ru>
2020-12-06 10:39:12 +0700

    Random spawn animals

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] animalPrefabs;
+
+    private float spawnRangeX = 10.0f;
+    private float spawnPositionZ = 20.0f;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            var animalIndex = Random.Range(0, animalPrefabs.Length);
+
+            var spawnPosition = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPositionZ);
+
+            Instantiate(animalPrefabs[animalIndex], spawnPosition, animalPrefabs[animalIndex].transform.rotation);
+        }
+    }
+}
